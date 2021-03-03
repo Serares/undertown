ea1006d5f9777a228aa7f879585d6a6d0f7b136a
Rares <jianu.rares30@gmail.com>
2021-03-03 09:07:09 +0200

    chore: views structure change, App changes for db separation

--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,23 @@
 import express, { Request, Response, NextFunction } from "express";
 import { createProxyMiddleware } from "http-proxy-middleware";
 import path from "path";
-import MongoDBStore from "connect-mongodb-session";
 import flash from "connect-flash";
 import helmet from "helmet";
-//
 import compression from "compression";
 // import { AuthRouter } from "./routes/auth";
 import { ErrorRouter } from "./routes/error";
 import { HomeRouter } from "./routes/home";
-import { MONGO_DB, MONGO_DB_SESSION_DB, GCS_BUCKET } from "./utils/secrets";
+import { GCS_BUCKET } from "./utils/secrets";
 
 //TODO implement Jest testing
 export class App {
     private _app: express.Application;
-    private _MongoURI: string = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0-xyshh.mongodb.net/${MONGO_DB}`;
     private errorRouter: ErrorRouter = new ErrorRouter();
     private homeRouter: HomeRouter;
     // private authRouter: AuthRouter;
 
     get app(): express.Application {
         return this._app;
-    }
-
-    get MongoURI(): string {
-        return this._MongoURI;
     }
 
     constructor() {
@@ -40,21 +33,6 @@ export class App {
     }
 
     private config(): void {
-        const tomorrowDate = new Date();
-        tomorrowDate.setDate(new Date().getDate() + 1);
-        // use plug-ins for the app
-        //configuration for sessions and cookies
-        //TODO make all callback functions private methods
-        this._app.use(session({
-            secret: process.env.SESSION_SECRET,
-            cookie: {
-                maxAge: 1000 * 60 * 90,
-                expires: tomorrowDate
-            },
-            resave: false,
-            saveUninitialized: false,
-            store: this.store
-        }));
 
         this._app.use((req: Request, res: Response, next: NextFunction) => {
             //DON'T FORGET TO UNCOMMENT THIS IN PRODUCTION
@@ -86,28 +64,6 @@ export class App {
         this._app.use(compression());
 
         this._app.use(flash());
-        //TODO define Types for parameters
-        this._app.use((req: any, res, next) => {
-            let userStatus = null;
-            if (req.session.user) {
-                userStatus = req.session.user.status;
-            }
-            // all rendered views get those variables
-            res.locals.isAuthenticated = req.session.isLoggedIn;
-            res.locals.userStatus = userStatus;
-            next();
-        });
-
-        //we need to add this middleware
-        //TODO create the callback function as a middleware method
-        //TODO I have to define a request Interface that has session object
-        this._app.use((req: any, res: Response, next: NextFunction) => {
-            //if there is no authentication then just ignore this middleware return next()
-            if (!req.session.user) {
-                return next();
-            }
-            // use redis to add authentication sessions
-        });
 
         // TODO add this error handler in error controller
         this._app.use((error: any, req: Request, res: Response, next: NextFunction) => {
