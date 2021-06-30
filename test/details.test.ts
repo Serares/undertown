37591f029a5903dd1def44f8da2ee2e91da9e0c8
Rares <jianu.rares30@gmail.com>
2021-06-30 17:07:44 +0300

    Modificare pentru test

--- test/details.test.ts
+++ test/details.test.ts
@@ -30,7 +30,7 @@ import { TransactionTypes, PropertyTypes } from "../src/modelView/values";
 
 describe(`GET single_page_details /chirii-:propertyType`, () => {
     it("Should return 500 for apartments with fake id", (done) => {
-        request(app).get(`/${TransactionTypes.RENT.endpoint}-${PropertyTypes.APARTMENTS.endpoint}/fakeId`).expect(200, done);
+        request(app).get(`/${TransactionTypes.RENT.endpoint}-${PropertyTypes.APARTMENTS.endpoint}/fakeId`).expect(500, done);
     });
     it("Should return 500 for houses with fake id", (done) => {
         request(app).get(`/${TransactionTypes.RENT.endpoint}-${PropertyTypes.HOUSE.endpoint}/fakeId`).expect(500, done);
